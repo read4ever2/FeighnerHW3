510aa3d75e1a48adb8018bf73f70d3f47c085e50
willfeighner <read4ever2@gmail.com>
2021-11-03 19:43:39 -0500

    update

--- Main.java
+++ Main.java
@@ -1,8 +1,41 @@
-class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            LoopPrinter threadPrinter = new LoopPrinter(Integer.toString(i));
-            threadPrinter.start();
-        }
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Thread[] threadArray = new Thread[3];
+    threadArray[0]= new Thread(new LoopPrinter(1));
+    threadArray[1]= new Thread(new LoopPrinter(2));
+    threadArray[2]= new Thread(new LoopPrinter(3));
+
+    for (Thread thread : threadArray) {
+      thread.start();
+      try {
+        Thread.sleep(25);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
+  }
+}
+
+class LoopPrinter implements Runnable {
+
+  private final int identifier;
+
+  LoopPrinter(int identifier) {
+    this.identifier = identifier;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 1; i < 6; i++) {
+      System.out.print("Thread " + identifier + " -- iteration " + i + "\n");
+      try {
+        Thread.sleep(50);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
